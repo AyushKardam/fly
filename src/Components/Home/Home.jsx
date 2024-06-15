622c22e3ef7089a74f34c43af0db0620a7df76e9
Ashu2607 <ashutosh.snh@iitg.ac.in>
2024-06-15 20:34:42 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+//imported assests =======>
+  import video from '../../assets/video.mp4'
+import aeroplane from '../../assets/tafeOff.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home flex container'>
+
+      <div className='mainText'>
+        <h1>Create Ever-Lasting Memories With Us</h1>
+      </div>
+
+
+      <div className="homeImages flex"> 
+        
+        <div className="videoDiv">
+          <video src={Video} autoplay muted loop className='Video'></video>
+        </div>
+        
+        <img src={aeroplane} className='Plane' />
+      </div>
+    </div>
   )
 }
 
