eb80c64b601458ac0c88cc8d15128d394c97eea6
Ashu2607 <ashutosh.snh@iitg.ac.in>
2024-06-15 20:33:50 +0530

    Update Lounge.jsx

--- src/Components/Lounge/Lounge.jsx
+++ src/Components/Lounge/Lounge.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+//imported imagrs========>
+import imageGrid from '../../assets/images-Grid2.png'
 
 const Lounge = () => {
   return (
-    <div>Lounge</div>
+    <div className='lounge container section'>
+      <div className="sectionContainer grid">
+
+        <div className="imgDiv">
+          <img src={imageGrid}  />
+        </div>
+
+
+      <div className="textDiv">
+        <h2>Unaccompanied Minor Lounge</h2>
+      </div>
+
+      <div className="grids grid">
+
+        <div className="singlrGrid">
+          <span className='gridTitle'>
+            Help through the airport
+          </span>
+          <p>
+            you can also call airlines from your phone and book a flight ticket 
+            to one of your favourite destinations.
+          </p>
+        </div>
+
+        
+        <div className="singlrGrid">
+          <span className='gridTitle'>
+            Priority Boarding
+          </span>
+          <p>
+            you can also call airlines from your phone and book a flight ticket 
+            to one of your favourite destinations.
+          </p>
+        </div>
+
+        
+        <div className="singlrGrid">
+          <span className='gridTitle'>
+           Care on the flight
+          </span>
+          <p>
+            you can also call airlines from your phone and book a flight ticket 
+            to one of your favourite destinations.
+          </p>
+        </div>
+
+        <div className="singlrGrid">
+          <span className='gridTitle'>
+            Chauffeur-drive service
+          </span>
+          <p>
+            you can also call airlines from your phone and book a flight ticket 
+            to one of your favourite destinations.
+          </p>
+        </div>
+
+      </div>
+      </div>
+
+    </div>
   )
 }
 
