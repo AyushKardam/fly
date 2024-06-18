2fcb51132cfb144915e7b139d24e1d55da74cca4
Adarsh-Kumar-2003 <152998538+Adarsh-Kumar-2003@users.noreply.github.com>
2024-06-18 07:37:55 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 //imported assets
 import video from '../../assets/video.mp4'
 import aeroplane from '../../assets/takeOff.png'
 
+//Import AOS
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Home = () => {
+  //use effect to set animations duration
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
   return (
     <div className='home flex container'>
       <div className="mainText">
-        <h1>Create a Bucket of Memories With Us</h1>
+        <h1 data-aos = 'fade-up' data-aos-duration = '2500'>Create a Bucket of Memories With Us</h1>
       </div>
 
       <div className="homeImages flex">
